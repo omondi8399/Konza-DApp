f519494dd42f796b635c950a861b4c49ccfbae95
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-01 17:01:02 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import Loading from "./components/Loading"
-import Alert from "./components/Alert"
-import Artworks from "./components/Artworks"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Hero from "./components/Hero"
+import { useEffect } from 'react'
+import { useGlobalState } from './store'
+import { isWallectConnected, loadNfts } from './Rodgers'
+import Alert from './components/Alert'
+import Artworks from './components/Artworks'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Loading from './components/Loading'
 
 const App = () => {
+  const [nfts] = useGlobalState('nfts')
+
+  useEffect(async () => {
+    await isWallectConnected().then(() => console.log('Blockchain Loaded'))
+    await loadNfts()
+  }, [])
+
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-hero">
         <Header />
         <Hero />
       </div>
-      <Artworks />
+      <Artworks artworks={nfts} />
       <Footer />
-      <Alert />
       <Loading />
+      <Alert />
     </div>
   )
 }
