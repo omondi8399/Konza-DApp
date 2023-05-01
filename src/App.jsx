fd475c2489987681fab9aa0af2cee0a020b642cc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-01 11:16:14 +0300

    Updated  src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Header from "./components/Header"
 const App = () => {
   return (
     <div className="min-h-screen">
-      <Header />
+      <div className="gradient-bg-hero">
+        <Header />
+      </div>
     </div>
   )
 }
